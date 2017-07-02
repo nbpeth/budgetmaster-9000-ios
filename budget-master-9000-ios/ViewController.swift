02878d4b1b1c8f5991385c14edb6c8f84861a55c
bud manstrong <nbpeth@gmail.com>
2017-07-02 13:53:15 -0500

    Initial Commit

---
+++ budget-master-9000-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  budget-master-9000-ios
+//
+//  Created by Bud Manstrong on 7/2/17.
+//  Copyright © 2017 Bud Manstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
