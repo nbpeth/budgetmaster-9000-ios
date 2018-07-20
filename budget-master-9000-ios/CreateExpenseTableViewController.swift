d24327980a41df35417ddb57fc498b0225ec0d77
Nicholas Peth <budmanstrong@Nicholass-MacBook-Pro.local>
2018-07-20 12:04:14 -0600

    ready

--- budget-master-9000-ios/CreateExpenseTableViewController.swift
+++ budget-master-9000-ios/CreateExpenseTableViewController.swift
@@ -81,8 +81,9 @@ class CreateExpenseTableViewController: BaseTableViewController, UIPickerViewDel
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+        
         [descriptionTextField, costTextField, locationTextField].forEach {
-            $0?.resignFirstResponder()
+            $0?.endEditing(true)
         }
     }
     
@@ -163,5 +164,41 @@ class CreateExpenseTableViewController: BaseTableViewController, UIPickerViewDel
         expenseTypeDetailLabel.text = expenseTypes[0].name ?? ""
         descriptionTextField.text = ""
         self.tableView.reloadData()
+    }
+}
+
+import Foundation
+import UIKit
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    func doneButtonAction()
+    {
+        self.resignFirstResponder()
     }
 }
